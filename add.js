0f492c09ebb42e7f3ee139865d7ba1081a2bbf9c
Ronit <ronyo1527@gmail.com>
2023-03-03 14:25:48 +0530

    adding user details in table,Save in local storage

---
+++ add.js
@@ -0,0 +1,87 @@
+const btn3 = document.querySelector('.button3')
+const btn = document.querySelector('.button')
+const cancel = document.querySelector('.button5')
+
+const userEntries = [];
+
+document.getElementById("submit").onclick = function () {
+    // document.getElementById("table").style.display = "block";
+    const table = document.getElementById("table");
+    const row = table.insertRow(-1);
+    userName = row.insertCell(0);
+    email = row.insertCell(1);
+    role = row.insertCell(2);
+    userName.innerHTML = document.getElementById("userName").value;
+    email.innerHTML = document.getElementById("email").value;
+    role.innerHTML = document.getElementById("role").value;
+
+
+    const entry = {
+        id: window.crypto.randomUUID(), // generate unique id for each entry
+        userName: document.getElementById("userName").value,
+        email: document.getElementById("email").value,
+        role: document.getElementById("role").value
+    };
+
+    userEntries.push(entry);
+    
+    return false
+}
+
+
+const hideform = document.querySelector('.hideform')
+btn3.addEventListener('click', function () {
+    hideform.style.display = "block";
+})
+
+const hideFormAgain = document.querySelector('.hideform')
+btn.addEventListener('click', function () {
+    hideform.style.display = "none";
+    resetForm()
+    handleStoreInLocal()
+}
+
+)
+const cancelAddUser = document.querySelector('.hideform')
+cancel.addEventListener('click', function () {
+    hideform.style.display = "none";
+})
+
+function resetForm() {
+    document.getElementById("userName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("role").value = "";
+    selectedRow = null;
+}
+
+function handleStoreInLocal() {
+
+    const userdata = JSON.stringify(userEntries)
+    localStorage.setItem("userEntries" , userdata)                                                                                                                         
+    
+}
+
+function onLoad() {
+
+    const userdata = localStorage.getItem("userEntries")
+    const fetcheddata= JSON.parse(userdata)
+
+    
+    const datavalues = fetcheddata.values();
+    for (const value of datavalues) {
+        userEntries.push(value);
+        console.log(value);
+        const table = document.getElementById("table");
+        const row = table.insertRow(-1);
+        userName = row.insertCell(0);
+        email = row.insertCell(1);
+        role = row.insertCell(2);
+        userName.innerHTML = value.userName;
+        email.innerHTML = value.email;
+        role.innerHTML = value.role;
+        
+      }
+      
+}
+
+document.addEventListener('DOMContentLoaded', onLoad);

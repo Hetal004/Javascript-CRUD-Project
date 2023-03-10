7d2b4813f1dafbba09ea52c9fd3b0d29bbe2c838
Hetal <hetalgn@gmail.com>
2023-03-10 12:00:39 +0530

    feat edit added

--- add.js
+++ add.js
@@ -1,3 +1,7 @@
+import { updateUserdata, onEdit } from './edit.js';
+import {onDelete} from './delete.js';
+// const updateUserdata = require("./edit.js");
+
 const btn3 = document.querySelector('.button3')
 const btn = document.querySelector('.button')
 const cancel = document.querySelector('.button5')
@@ -5,83 +9,108 @@ const cancel = document.querySelector('.button5')
 const userEntries = [];
 
 document.getElementById("submit").onclick = function () {
-    // document.getElementById("table").style.display = "block";
-    const table = document.getElementById("table");
-    const row = table.insertRow(-1);
-    userName = row.insertCell(0);
-    email = row.insertCell(1);
-    role = row.insertCell(2);
-    userName.innerHTML = document.getElementById("userName").value;
-    email.innerHTML = document.getElementById("email").value;
-    role.innerHTML = document.getElementById("role").value;
+    let tmpid = document.getElementById("uid").value;
+    console.log(tmpid);
+    if (tmpid == "") {
+        console.log("submit ...");
+        const table = document.getElementById("table");
+        const row = table.insertRow(-1);
+        const entry = {
+            id: window.crypto.randomUUID(), // generate unique id for each entry
+            userName: document.getElementById("userName").value,
+            email: document.getElementById("email").value,
+            role: document.getElementById("role").value
+        };
+        const id = row.insertCell(0);
+        const userName = row.insertCell(1);
+        const email = row.insertCell(2);
+        const role = row.insertCell(3);
+        const action = row.insertCell(4);
 
+        id.innerHTML = entry.id;
+        userName.innerHTML = entry.userName;
+        email.innerHTML = entry.email;
+        role.innerHTML = entry.role;
+        const editButton = document.createElement("button");
+        editButton.addEventListener("click", onEdit);
+        action.appendChild(editButton);
+        // action.innerHTML = `<button onClick="${onEdit(this)}">Edit</button>
+        //     <button onclick="${onDelete(this)}">Delete</button>`;
 
-    const entry = {
-        id: window.crypto.randomUUID(), // generate unique id for each entry
-        userName: document.getElementById("userName").value,
-        email: document.getElementById("email").value,
-        role: document.getElementById("role").value
-    };
-
-    userEntries.push(entry);
-    
+        userEntries.push(entry);
+        handleStoreInLocal()
+    }
+    else {
+        console.log(tmpid);
+        updateUserdata(tmpid)
+    }
+    // const hideFormAgain = document.querySelector('.hideform')
+    hideform.style.display = "none";
     return false
 }
-
 
 const hideform = document.querySelector('.hideform')
 btn3.addEventListener('click', function () {
     hideform.style.display = "block";
+    resetForm();
 })
 
-const hideFormAgain = document.querySelector('.hideform')
-btn.addEventListener('click', function () {
-    hideform.style.display = "none";
-    resetForm()
-    handleStoreInLocal()
-}
-
-)
 const cancelAddUser = document.querySelector('.hideform')
 cancel.addEventListener('click', function () {
     hideform.style.display = "none";
 })
 
 function resetForm() {
+    document.getElementById("uid").value = "";
     document.getElementById("userName").value = "";
     document.getElementById("email").value = "";
     document.getElementById("role").value = "";
-    selectedRow = null;
+    // selectedRow = null;
 }
 
-function handleStoreInLocal() {
+export function handleStoreInLocal() {
 
     const userdata = JSON.stringify(userEntries)
-    localStorage.setItem("userEntries" , userdata)                                                                                                                         
-    
+    localStorage.setItem("userEntries", userdata)
 }
 
-function onLoad() {
-
+export function onLoad() {
+    console.log("onload...");
     const userdata = localStorage.getItem("userEntries")
-    const fetcheddata= JSON.parse(userdata)
+    const fetcheddata = JSON.parse(userdata)
 
-    
+    if (fetcheddata == null) return;
     const datavalues = fetcheddata.values();
     for (const value of datavalues) {
         userEntries.push(value);
         console.log(value);
         const table = document.getElementById("table");
         const row = table.insertRow(-1);
-        userName = row.insertCell(0);
-        email = row.insertCell(1);
-        role = row.insertCell(2);
+
+        const id = row.insertCell(0);
+        const userName = row.insertCell(1);
+        const email = row.insertCell(2);
+        const role = row.insertCell(3);
+        const action = row.insertCell(4);
+
+        id.innerHTML = value.id;
         userName.innerHTML = value.userName;
         email.innerHTML = value.email;
         role.innerHTML = value.role;
-        
-      }
-      
-}
 
+        const editButton = document.createElement("button");
+        const textForEditButton = document.createTextNode("Edit");
+        editButton.appendChild(textForEditButton);
+        editButton.addEventListener("click", onEdit);
+        action.appendChild(editButton);
+
+        const deleteButton = document.createElement("button");
+        const textForDeleteButton = document.createTextNode("Delete");
+        deleteButton.appendChild(textForDeleteButton);
+        deleteButton.addEventListener("click", onDelete);
+        action.appendChild(deleteButton);
+
+    }
+}
 document.addEventListener('DOMContentLoaded', onLoad);
+
